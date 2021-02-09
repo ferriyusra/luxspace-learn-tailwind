1ff93c2c54dcce2fb2b08cf3addbf4beab7685a7
John Smith <ferisakuragi@gmail.com>
2021-02-09 13:16:41 +0700

    - membuat breadcrumb dan details product
    - membuat details product dan juga breadcrumb serta implemetnasi responsive ketika layar mobile

--- src/js/carousel.js
+++ src/js/carousel.js
@@ -1,8 +1,8 @@
 import { addClass, removeClass } from './utils-class'
 
-const carouselId = document.getElementById("carousel")
-const carouselItems = carouselId.getElementsByClassName("flex")[0]
-const carouselContainer = carouselId.getElementsByClassName("container")[0]
+const carouselId = document?.getElementById("carousel")
+const carouselItems = carouselId?.getElementsByClassName("flex")[0]
+const carouselContainer = carouselId?.getElementsByClassName("container")[0]
 
 function carouselCalculateOffset() {
     const carouselOffset = carouselContainer.getBoundingClientRect().left;
