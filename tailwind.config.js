327db559a1f4eeb91f9a6199e847679a12d048bc
John Smith <ferisakuragi@gmail.com>
2021-02-09 16:25:41 +0700

    - membuat halaman cart
    - membuat section shopping cart

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,7 @@ module.exports = {
       black: colors.black,
       white: colors.white,
       gray: { ...colors.coolGray, 100: '#F9F9F9', 200: '#F1F1F1' },
-      red: colors.red,
+      red: { ...colors.red, 600: '#E0195D' },
       yellow: colors.amber,
       green: colors.emerald,
       blue: { ...colors.blue, 200: '#BCE7F0' },
